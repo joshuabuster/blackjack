f187c3a74dbb26aa77c9d5f6d2b1589de5ab9602
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-08-08 19:53:47 -0700

    adds psuedocode to js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Blackjack Project
+
+// ========  Psueocode  =========
+// initalize deck
+// deal two random cards to player and dealer
+    // both face up for player and one face one face down for dealer
+// if player has blackjack move to dealer play.
+// player received message of currect total and dealers total and is asked to hit? or stay?
+// if hit 
+    // player receives another card face up
+        // player bust
+        // if 21 and moves to dealer and recieves message win, lose or draw
+    // player prompted again hit or stay
+// if stay
+    // moves to dealer to dealer play
+    // player revcieves message win, lose, draw
+// player balance updated
+// recieves message play again?
+
 /*----- constants -----*/
 
 
